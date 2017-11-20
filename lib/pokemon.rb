e1b9ff90990fb8c4ff41757268511d7e33b449ad
Kristen Peck <kpeck71@gmail.com>
2017-11-20 23:43:20 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -2,10 +2,10 @@ class Pokemon
   # attr_accessor :name, :type, :db
 
   def initialize(catch_em_all)
+    @id = id
     @name = catch_em_all[:pk_name]
     @type = catch_em_all[:pk_type]
     @db = catch_em_all[:db]
-    @id = catch_em_all[:index]
   end
 
   def self.save(pk_name, pk_type, db)
