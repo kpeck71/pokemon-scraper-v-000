4b68bb32cb5ab9ec5d28075c2e32be6d6a356de3
Kristen Peck <kpeck71@gmail.com>
2017-11-20 23:48:58 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -5,7 +5,7 @@ binding.pry
     @id = catch_em_all[:find_index]
     @name = catch_em_all[:pk_name]
     @type = catch_em_all[:pk_type]
-    @db = catch_em_all[:db]
+    @db = catch_em_all[i]
   end
 
   def self.save(pk_name, pk_type, db)
