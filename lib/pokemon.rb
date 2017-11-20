dfa4581d6bd94310b3532bf964cd9b688243c501
Kristen Peck <kpeck71@gmail.com>
2017-11-20 23:35:45 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,13 +1,13 @@
 class Pokemon
   attr_accessor :name, :type, :db
-  
-  def initialize(name,type,db)
+
+  def initialize(name, type, db)
     @name = name
     @type = type
     @db = db
   end
 
-  def save
+  def save(pk_name, pk_type, db)
   end
 
   def find
