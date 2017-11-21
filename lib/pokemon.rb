dd6e48b0e7db3b6914ccc96d7d459ff51f7ed9e6
Kristen Peck <kpeck71@gmail.com>
2017-11-21 01:22:40 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 
   @@all = []
 
-  def initialize(catch_em_all)
+  def initialize(catch_em_all=[])
     @id = catch_em_all["id"]
     @name = catch_em_all["name"]
     @type catch_em_all["type"]
