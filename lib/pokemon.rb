ca8470db6fd6a01d0d3f59ed84a21dd65370dd74
Kristen Peck <kpeck71@gmail.com>
2017-11-21 00:32:14 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
   def self.all
     @@all
   end
-
+  binding.pry
   def self.save(name, type, hp, db)
     db.execute("INSERT INTO pokemon (name, type, hp) VALUES (?, ?)", name, type, 60)
   end
