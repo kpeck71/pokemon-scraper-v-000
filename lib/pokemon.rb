ad2390028c1b9e762b7c9b915d907d2db2ae3f49
Kristen Peck <kpeck71@gmail.com>
2017-11-21 01:11:26 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -3,7 +3,7 @@ class Pokemon
 
   @@all = []
 
-  def initialize(:id, :name, :type, :db, :hp)
+  def initialize(:id, :name, :type, :db, hp:nil)
     @id, @name, @type, @db, @hp = id, name, type, db, hp
 
 
